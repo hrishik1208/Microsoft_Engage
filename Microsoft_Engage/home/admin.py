535c285ac797ad55e1348ac99cbbdf57bc0ed2b2
Hrishik <hrishikrajeshkanade@iitbhilai.ac.in>
2022-05-22 19:49:20 +0530

    Student Profile Page Created

---
+++ Microsoft_Engage/home/admin.py
@@ -0,0 +1,28 @@
+from atexit import register
+from django.contrib import admin
+from home.models import contact
+from home.models import Mains
+from home.models import Student_reg
+from home.models import Teacher_reg
+from home.models import Course
+from home.models import Live
+from home.models import Course_str
+from home.models import Non_approved
+from home.models import Approved
+from home.models import Student_attendace_report
+from home.models import recognize
+from home.models import Join
+# Register your models here.
+
+admin.site.register(contact)
+admin.site.register(Mains)
+admin.site.register(Teacher_reg)
+admin.site.register(Student_reg)
+admin.site.register(Course)
+admin.site.register(Live)
+admin.site.register(Course_str)
+admin.site.register(Non_approved)
+admin.site.register(Approved)
+admin.site.register(Student_attendace_report)
+admin.site.register(recognize)
+admin.site.register(Join)
---
+++ Microsoft_Engage/home/models.py
@@ -0,0 +1,140 @@
+from pyexpat import model
+from tkinter import Image
+from unicodedata import name
+from django.db import models
+from datetime import datetime
+import os
+
+from django.forms import ImageField, IntegerField
+# Create your models here.
+class contact(models.Model):
+    name=models.CharField(max_length=100,default="kshh")
+    email=models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+    # date=models.DateField()
+    # check=models.CharField()
+
+    def __str__(self):
+        return self.name
+class Teacher_reg(models.Model):
+    name=models.CharField(max_length=100,default="kshh")
+    username=models.CharField(max_length=100,default="kshh")
+    email=models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+    # date=models.DateField()
+    # check=models.CharField()
+
+    def __str__(self):
+        return self.name
+
+class Student_reg(models.Model):
+    name=models.CharField(max_length=100)
+    username=models.CharField(max_length=100)
+    email=models.CharField(max_length=100)
+    password=models.CharField(max_length=100)
+    # date=models.DateField()
+    # check=models.CharField()
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model): 
+    username=models.CharField(max_length=100) 
+    course_name=models.CharField(max_length=100) 
+    join_code=models.IntegerField()
+    # date=models.DateField()
+    # check=models.CharField()
+
+    def __str__(self):
+        return self.course_name
+
+
+class Live(models.Model):
+    username=models.CharField(max_length=100)
+    course_name=models.CharField(max_length=100)
+    time_future=models.IntegerField()
+    time_present=models.IntegerField(default=0)
+    attended_list=models.TextField(null=True)
+    date=models.DateField(default="2021-10-10")
+    latitude=models.DecimalField(max_digits=19,decimal_places=15,default=0)
+    longitude=models.DecimalField(max_digits=19,decimal_places=15,default=0)
+    bool = models.IntegerField(default=0)
+    radius = models.IntegerField(default=0,)
+    # date=models.DateField()
+    # check=models.CharField()
+    class Meta:
+        ordering = ('date',)
+
+    def __str__(self):
+        return self.course_name
+
+class Course_str(models.Model):
+    username=models.CharField(max_length=100)
+    course_name=models.CharField(max_length=100)
+    date=models.DateField(default="2021-10-10")
+    time_present=models.IntegerField(default=0)
+    attended_list=models.TextField(null=True)
+    class Meta:
+        ordering = ('date',)
+    
+    def __str__(self):
+        return self.course_name
+
+class Non_approved(models.Model):
+    t_username=models.CharField(max_length=100)
+    t_name=models.CharField(max_length=100)
+    s_username=models.CharField(max_length=100)
+    s_id=models.IntegerField(default="10000000")
+    s_email=models.CharField(max_length=100)
+    course_name=models.CharField(max_length=100)
+    img=models.ImageField(null=True,blank=True)
+    join_code=models.IntegerField()
+
+    def __str__(self):
+        return self.course_name
+
+class Approved(models.Model):
+    t_username=models.CharField(max_length=100)
+    t_name=models.CharField(max_length=100)
+    s_username=models.CharField(max_length=100)
+    s_id=models.IntegerField(default="10000000")
+    s_email=models.CharField(max_length=100)
+    course_name=models.CharField(max_length=100)
+    img=models.ImageField(null=True,blank=True)
+    join_code=models.IntegerField()
+
+    def __str__(self):
+        return self.course_name
+
+
+class Student_attendace_report(models.Model):
+    t_username=models.CharField(max_length=100)
+    s_username=models.CharField(max_length=100)
+    course_name=models.CharField(max_length=100)
+    date=models.DateField(default="2021-10-10")
+    bool=models.IntegerField(default=0)
+    time_present=models.IntegerField()
+
+    class Meta:
+        ordering = ('date',)
+
+    def __str__(self):
+        return self.course_name
+
+class Join(models.Model):
+    username=models.CharField(max_length=100,primary_key=True)
+    If_posted=models.IntegerField(default=0)
+    Response_charge=models.IntegerField(default=0)
+
+class recognize(models.Model):
+    username=models.CharField(max_length=100,primary_key=True)
+    If_posted=models.IntegerField(default=0)
+    Response_charge=models.IntegerField(default=0)
+
+class Mains(models.Model):
+    name=models.CharField(max_length=100,default="kshh")
+    img=models.ImageField(null=True,blank=True)
+
+    def __str__(self):
+        return self.name
+ 
